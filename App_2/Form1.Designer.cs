5115a2a569591232245b031aba5444df40a59e3f
AdrianS2002 <aschiop2002@yahoo.com>
2023-02-04 21:23:59 +0200

    first commit

---
+++ App_2/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace App_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btn_refresh = new System.Windows.Forms.Button();
+            this.btn_farward = new System.Windows.Forms.Button();
+            this.btn_back = new System.Windows.Forms.Button();
+            this.btn_home = new System.Windows.Forms.Button();
+            this.btn_search = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.Location = new System.Drawing.Point(24, 17);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(684, 38);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // btn_refresh
+            // 
+            this.btn_refresh.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_refresh.BackColor = System.Drawing.Color.Transparent;
+            this.btn_refresh.BackgroundImage = global::App_2.Properties.Resources._61225;
+            this.btn_refresh.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_refresh.Location = new System.Drawing.Point(945, 19);
+            this.btn_refresh.Name = "btn_refresh";
+            this.btn_refresh.Size = new System.Drawing.Size(50, 40);
+            this.btn_refresh.TabIndex = 5;
+            this.btn_refresh.UseVisualStyleBackColor = false;
+            // 
+            // btn_farward
+            // 
+            this.btn_farward.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_farward.BackColor = System.Drawing.Color.Transparent;
+            this.btn_farward.BackgroundImage = global::App_2.Properties.Resources.images;
+            this.btn_farward.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_farward.Location = new System.Drawing.Point(889, 19);
+            this.btn_farward.Name = "btn_farward";
+            this.btn_farward.Size = new System.Drawing.Size(50, 40);
+            this.btn_farward.TabIndex = 4;
+            this.btn_farward.UseVisualStyleBackColor = false;
+            this.btn_farward.Click += new System.EventHandler(this.btn_farward_Click);
+            // 
+            // btn_back
+            // 
+            this.btn_back.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_back.BackColor = System.Drawing.Color.Transparent;
+            this.btn_back.BackgroundImage = global::App_2.Properties.Resources.back;
+            this.btn_back.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_back.Location = new System.Drawing.Point(833, 18);
+            this.btn_back.Name = "btn_back";
+            this.btn_back.Size = new System.Drawing.Size(50, 40);
+            this.btn_back.TabIndex = 3;
+            this.btn_back.UseVisualStyleBackColor = false;
+            this.btn_back.Click += new System.EventHandler(this.btn_back_Click);
+            // 
+            // btn_home
+            // 
+            this.btn_home.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_home.BackColor = System.Drawing.Color.Transparent;
+            this.btn_home.BackgroundImage = global::App_2.Properties.Resources.home;
+            this.btn_home.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_home.Location = new System.Drawing.Point(777, 18);
+            this.btn_home.Name = "btn_home";
+            this.btn_home.Size = new System.Drawing.Size(50, 40);
+            this.btn_home.TabIndex = 2;
+            this.btn_home.UseVisualStyleBackColor = false;
+            this.btn_home.Click += new System.EventHandler(this.btn_home_Click);
+            // 
+            // btn_search
+            // 
+            this.btn_search.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_search.BackColor = System.Drawing.Color.Transparent;
+            this.btn_search.BackgroundImage = global::App_2.Properties.Resources._591_5912453_navigation_bar_icons_png_bar_icon_search_png;
+            this.btn_search.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_search.Location = new System.Drawing.Point(714, 18);
+            this.btn_search.Name = "btn_search";
+            this.btn_search.Size = new System.Drawing.Size(57, 40);
+            this.btn_search.TabIndex = 1;
+            this.btn_search.UseVisualStyleBackColor = false;
+            this.btn_search.Click += new System.EventHandler(this.btn_search_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser1.Location = new System.Drawing.Point(24, 67);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(969, 399);
+            this.webBrowser1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(1005, 487);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.btn_refresh);
+            this.Controls.Add(this.btn_farward);
+            this.Controls.Add(this.btn_back);
+            this.Controls.Add(this.btn_home);
+            this.Controls.Add(this.btn_search);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Browser";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btn_search;
+        private System.Windows.Forms.Button btn_home;
+        private System.Windows.Forms.Button btn_back;
+        private System.Windows.Forms.Button btn_farward;
+        private System.Windows.Forms.Button btn_refresh;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ App_2/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        String adresa,veche;
+
+        private void btn_search_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate(textBox1.Text);
+            adresa = textBox1.Text;
+        }
+
+        private void btn_back_Click(object sender, EventArgs e)
+        {
+           // webBrowser1.GoBack();
+            webBrowser1.Navigate(adresa);
+        }
+
+        private void btn_farward_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoForward();
+        }
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                webBrowser1.Navigate(textBox1.Text);
+        }
+
+
+        private void btn_home_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("bing.com");
+        }
+      
+    }
+}
